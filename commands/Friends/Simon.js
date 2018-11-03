63dff145394493e5fd7483937dc4aef26097b8a8
Dylan Cornelison <44535062+Mrfreddycoding@users.noreply.github.com>
2018-11-03 16:19:28 -0400

    Add files via upload

---
+++ commands/Friends/Simon.js
@@ -0,0 +1,21 @@
+const commando = require('discord.js-commando');
+
+class SimonFriendCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'Simon',
+            group: 'friends',
+            memberName: 'simon',
+            description: "Describes a good of the bot's owner"
+        })
+    }
+
+    async run(message, args)
+    {
+        message.channel.send("Simon is a amazing dude who loves Gianny very much!")
+    }
+}
+
+module.exports = SimonFriendCommand;
