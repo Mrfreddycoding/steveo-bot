145ee16186786548cdd3d38fe16e833be35222b4
Dylan Cornelison <44535062+Mrfreddycoding@users.noreply.github.com>
2018-11-06 16:54:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const Commando = require('discord.js-commando');
-const bot = new Commando.Client({
-    commandPrefix: '*',
-    owner: '269495733613363200'
-});
+const bot = new Commando.Client();
+const TOKEN = 'NTAzMzExNzEyODkyNjE2NzE4.Dr5wrA.Ik9eiabNlMZUDD4gBo8DViOaFUs'
 
+bot.registry.registerGroup('music', 'Music');
 bot.registry.registerGroup('friends', 'Friends');
 bot.registry.registerGroup('moderation', 'Moderation');
 bot.registry.registerGroup('random', 'Random');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + '/commands');
 
+global.servers = {};
+
 bot.on('ready',function(){
     console.log('Bot Online!')
 })
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(TOKEN);
